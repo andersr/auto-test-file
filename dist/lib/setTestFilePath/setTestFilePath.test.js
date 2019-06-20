96a157084eb489f01cfb32ee67878434a65b0873
Anders Ramsay <andersr@gmail.com>
2019-06-20 12:32:26 -0400

    exclude test files from dist

--- dist/lib/setTestFilePath/setTestFilePath.test.js
+++ dist/lib/setTestFilePath/setTestFilePath.test.js
@@ -1,7 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var setTestFilePath_1 = require("./setTestFilePath");
-// const removeWhiteSpace = (str: string) => str.replace(/\s+/g, ' ').trim();
 describe("setTestFilePath", function () {
     afterEach(function () {
         delete process.env.INIT_CWD;
@@ -9,9 +8,9 @@ describe("setTestFilePath", function () {
     it("it generates a full test file path", function () {
         var projectDir = "/Users/someuser/dev/my_project";
         var filePath = "src/myFile.ts";
+        var fileName = "myFile";
         process.env.INIT_CWD = projectDir;
         var expected = "/Users/someuser/dev/my_project/src/myFile.test.ts";
-        // const content = removeWhiteSpace(setTestFilePath(input));
-        expect(setTestFilePath_1.setTestFilePath(filePath)).toEqual(expected);
+        expect(setTestFilePath_1.setTestFilePath(filePath, fileName)).toEqual(expected);
     });
 });
--- src/lib/setTestFilePath/setTestFilePath.test.ts
+++ src/lib/setTestFilePath/setTestFilePath.test.ts
@@ -1,7 +1,5 @@
 import { setTestFilePath } from "./setTestFilePath";
 
-// const removeWhiteSpace = (str: string) => str.replace(/\s+/g, ' ').trim();
-
 describe("setTestFilePath", () => {
 
     afterEach(() => {
@@ -11,11 +9,11 @@ describe("setTestFilePath", () => {
     it("it generates a full test file path", () => {
         const projectDir = "/Users/someuser/dev/my_project";
         const filePath = "src/myFile.ts";
+        const fileName = "myFile";
         process.env.INIT_CWD = projectDir;
         const expected = "/Users/someuser/dev/my_project/src/myFile.test.ts";
 
-        // const content = removeWhiteSpace(setTestFilePath(input));
-        expect(setTestFilePath(filePath)).toEqual(expected);
+        expect(setTestFilePath(filePath, fileName)).toEqual(expected);
     });
 
 });
