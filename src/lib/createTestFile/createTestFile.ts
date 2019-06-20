9f25108cf977ae6b27321943ae4d7e635c2c844b
Anders Ramsay <andersr@gmail.com>
2019-06-20 11:18:05 -0400

    cleanup

--- src/lib/createTestFile/createTestFile.ts
+++ src/lib/createTestFile/createTestFile.ts
@@ -9,11 +9,9 @@ export const createTestFile = (filePath: string) => {
   const noExtension = removeExtension(fileName);
   const projectDir = process.env.INIT_CWD;
   const projectPath = `${projectDir}/${path.dirname(filePath)}`;
-
-  // TODO: create clear relationship between "test" value and ignore, to prevent infinite loop of creates
   const newFile = `${projectPath}/${noExtension}.test${extension}`;
 
-  createFile(newFile, setTestFileContent(noExtension), function(err: any) {
+  createFile(newFile, setTestFileContent(noExtension), function (err: any) {
     if (err) {
       console.log("err: ", err);
     }
